fff2af3cf3afdaa6b36bc68c0881acd7eb5432a1
joe-white09 <joe_white09@yahoo.com>
2022-07-14 14:46:04 -0500

    update and delete tag routes added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,6 +17,22 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // be sure to include its associated Category and Tag data
 });
 
